66788943aaca701e449aec19661d090a48ce7488
Saraswat37698 <jaiswalacharyya37698@hotmail.com>
2025-04-10 00:11:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename="/FinalMCT4">
       <App />
     </HashRouter>
   </StrictMode>
