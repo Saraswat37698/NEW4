82c0def58e8075a0da50a73fe1f56177fa71eb9e
Saraswat37698 <jaiswalacharyya37698@hotmail.com>
2025-04-10 00:35:13 +0530

    Changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { HashRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom"; // ✅ Important!
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-  <StrictMode>
-    <HashRouter basename="/FinalMCT4">
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
     </HashRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
